8a13f3938d847865e2319e9367c6056fcad603bc
ddennis <ddennis@CAS-DDENNIS-L7U.clydeinc.com>
2015-06-30 17:52:18 -0600

    added methods and javadoc comments for UserManager.java and User.java. Deleted DevCardList and ResourceList as DevCardType and ResourceType serve their purposes respectively

--- java/src/client/data/User.java
+++ java/src/client/data/User.java
@@ -2,4 +2,53 @@ package client.data;
 
 public class User {
 
+    private String username;
+    private String password;
+    
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User [name = " + username + ", password = *******]";
+    }
+
+    @Override
+    public int hashCode() {
+        return (username + password).hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        boolean condition = false;
+        if (obj == null) {
+            return condition;
+        }
+        if (getClass() != obj.getClass()) {
+            return condition;
+        }
+        User other = (User) obj;
+        if (username.equals(other.getUsername())) {
+            condition = true;
+        } 
+        return condition;
+    }
 }
