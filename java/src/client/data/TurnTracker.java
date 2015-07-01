46aaababedf98ccd0c570ff27285d000ae14a7cb
= <=>
2015-07-01 02:44:43 -0600

    Basic javadocs and stubbed classes for my portion

--- java/src/client/data/TurnTracker.java
+++ java/src/client/data/TurnTracker.java
@@ -2,4 +2,45 @@ package client.data;
 
 public class TurnTracker {
 
+	private int currentTurn;
+	private String status;
+	private int longestRoad;
+	private int largestArmy;
+	
+	public TurnTracker() {
+		
+	}
+
+	public int getCurrentTurn() {
+		return currentTurn;
+	}
+
+	public void setCurrentTurn(int currentTurn) {
+		this.currentTurn = currentTurn;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public int getLongestRoad() {
+		return longestRoad;
+	}
+
+	public void setLongestRoad(int longestRoad) {
+		this.longestRoad = longestRoad;
+	}
+
+	public int getLargestArmy() {
+		return largestArmy;
+	}
+
+	public void setLargestArmy(int largestArmy) {
+		this.largestArmy = largestArmy;
+	}
+	
 }
