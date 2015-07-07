a4853f9d86cdf7da23e094edc516ceece6c3864c
aaron monson <aaron@lmonson.com>
2015-07-06 18:38:37 -0600

    Added empty test classes.

---
+++ modeltests/client/managers/UserManagerTest.java
@@ -0,0 +1,15 @@
+package client.managers;
+
+import static org.junit.Assert.*;
+import client.managers.UserManager;
+
+import org.junit.Test;
+
+public class UserManagerTest {
+
+	@Test
+	public void testUserManager() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ modeltests/client/proxy/ServerProllerTest.java
@@ -0,0 +1,14 @@
+package client.proxy;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ServerProllerTest {
+
+	@Test
+	public void testMethod() {
+		fail("Not yet implemented");
+	}
+
+}
