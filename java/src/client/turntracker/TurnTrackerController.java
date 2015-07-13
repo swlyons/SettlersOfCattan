b0e307e92de1df712271e570cd43fa76f4760778
= <=>
2015-07-13 17:34:49 -0600

    Made the turn tracker controller

--- java/src/client/turntracker/TurnTrackerController.java
+++ java/src/client/turntracker/TurnTrackerController.java
@@ -2,7 +2,10 @@ package client.turntracker;
 
 import shared.definitions.CatanColor;
 import client.base.*;
-
+import client.communication.ClientCommunicator;
+import client.communication.ClientCommunicatorFascadeSettlersOfCatan;
+import client.proxy.FinishMove;
+import client.managers.GameManager;
 
 /**
  * Implementation for the turn tracker controller
@@ -24,7 +27,14 @@ public class TurnTrackerController extends Controller implements ITurnTrackerCon
 
 	@Override
 	public void endTurn() {
-
+		GameManager gm = ClientCommunicator.getSingleton().getGameManager();
+		FinishMove fm = new FinishMove(gm.getGame().getTurnTracker().getCurrentTurn());
+		try {
+			gm.initializeGame(ClientCommunicatorFascadeSettlersOfCatan.getSingleton().finishMove(fm));
+			getView().updateGameState("Waiting for other players", false);
+		} catch(Exception e) {
+			System.out.println("Couldn't end turn");
+		}		
 	}
 	
 	private void initFromModel() {
