9ce4c4c50e961fc9f72ae6bdeda84de01450725a
Samuel Lyons <PestytheRat@gmail.com>
2015-07-13 22:55:10 -0600

    add AI probably works now

--- java/src/client/join/PlayerWaitingController.java
+++ java/src/client/join/PlayerWaitingController.java
@@ -1,7 +1,11 @@
 package client.join;
 
 import client.base.*;
-
+import client.proxy.AddAIRequest;
+import client.communication.ClientCommunicator;
+import client.communication.ClientCommunicatorFascadeSettlersOfCatan;
+import client.managers.GameManager;
+import java.util.ArrayList;
 
 /**
  * Implementation for the player waiting controller
@@ -21,16 +25,33 @@ public class PlayerWaitingController extends Controller implements IPlayerWaitin
 
 	@Override
 	public void start() {
-
-		getView().showModal();
-	}
+            try{
+                ArrayList<String> aiTypes = ClientCommunicatorFascadeSettlersOfCatan.getSingleton().listAITypesInGame();
+                String[] aiTypes2 = new String[aiTypes.size()];
+                aiTypes.toArray(aiTypes2);
+                getView().setAIChoices(aiTypes2);
+            }catch(Exception e){
+            } finally{
+                getView().showModal();
+            }
+        }
 
 	@Override
 	public void addAI() {
-
-		// TEMPORARY
-		getView().closeModal();
-	}
+                AddAIRequest add = new AddAIRequest(getView().getSelectedAI());
+                System.out.println(add);
+                try{
+                    String responseBody = ClientCommunicatorFascadeSettlersOfCatan.getSingleton().addAIToGame(add);
+                    GameManager gm = ClientCommunicator.getSingleton().getGameManager();
+                    gm.initializeGame(responseBody);
+                    if(gm.getGame().getPlayers().size()==4){
+                        getView().closeModal();
+                    }
+                } catch (Exception e){
+                
+                }
+                
+        }
 
 }
 
