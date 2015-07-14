0abb9546e27c9c2a9b58f8ba3446f5b069a1a254
De-Wayne Dennis <ddennis@clydeinc.com>
2015-07-13 18:38:26 -0600

    made player id unique across games

--- java/src/client/join/JoinGameController.java
+++ java/src/client/join/JoinGameController.java
@@ -8,176 +8,196 @@ import client.communication.CookieModel;
 import client.data.*;
 import client.misc.*;
 import com.google.gson.GsonBuilder;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
-
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Implementation for the join game controller
  */
 public class JoinGameController extends Controller implements IJoinGameController {
 
-	private INewGameView newGameView;
-	private ISelectColorView selectColorView;
-	private IMessageView messageView;
-	private IAction joinAction;
-	private ArrayList<GameInfo> games = new ArrayList<>();
-        
-        int currentGameId = 0;
-	/**
-	 * JoinGameController constructor
-	 * 
-	 * @param view Join game view
-	 * @param newGameView New game view
-	 * @param selectColorView Select color view
-	 * @param messageView Message view (used to display error messages that occur while the user is joining a game)
-	 */
-	public JoinGameController(IJoinGameView view, INewGameView newGameView, 
-								ISelectColorView selectColorView, IMessageView messageView) {
+    private INewGameView newGameView;
+    private ISelectColorView selectColorView;
+    private IMessageView messageView;
+    private IAction joinAction;
+    private ArrayList<GameInfo> games = new ArrayList<>();
 
-		super(view);
+    int currentGameId = 0;
 
-		setNewGameView(newGameView);
-		setSelectColorView(selectColorView);
-		setMessageView(messageView);
-	}
-	
-	public IJoinGameView getJoinGameView() {
-		
-		return (IJoinGameView)super.getView();
-	}
-	
-	/**
-	 * Returns the action to be executed when the user joins a game
-	 * 
-	 * @return The action to be executed when the user joins a game
-	 */
-	public IAction getJoinAction() {
-		
-		return joinAction;
-	}
+    /**
+     * JoinGameController constructor
+     *
+     * @param view Join game view
+     * @param newGameView New game view
+     * @param selectColorView Select color view
+     * @param messageView Message view (used to display error messages that
+     * occur while the user is joining a game)
+     */
+    public JoinGameController(IJoinGameView view, INewGameView newGameView,
+            ISelectColorView selectColorView, IMessageView messageView) {
 
-	/**
-	 * Sets the action to be executed when the user joins a game
-	 * 
-	 * @param value The action to be executed when the user joins a game
-	 */
-	public void setJoinAction(IAction value) {	
-		
-		joinAction = value;
-	}
-	
-	public INewGameView getNewGameView() {
-		
-		return newGameView;
-	}
+        super(view);
 
-	public void setNewGameView(INewGameView newGameView) {
-		
-		this.newGameView = newGameView;
-	}
-	
-	public ISelectColorView getSelectColorView() {
-		
-		return selectColorView;
-	}
-	public void setSelectColorView(ISelectColorView selectColorView) {
-		
-		this.selectColorView = selectColorView;
-	}
-	
-	public IMessageView getMessageView() {
-		
-		return messageView;
-	}
-	public void setMessageView(IMessageView messageView) {
-		
-		this.messageView = messageView;
-	}
+        setNewGameView(newGameView);
+        setSelectColorView(selectColorView);
+        setMessageView(messageView);
+    }
 
-	@Override
-	public void start() {
-		
-		getJoinGameView().showModal();
-	}
+    public IJoinGameView getJoinGameView() {
 
-	@Override
-	public void startCreateNewGame() {
-		
-		getNewGameView().showModal();
-	}
+        return (IJoinGameView) super.getView();
+    }
 
-	@Override
-	public void cancelCreateNewGame() {
-		
-		getNewGameView().closeModal();
-	}
+    /**
+     * Returns the action to be executed when the user joins a game
+     *
+     * @return The action to be executed when the user joins a game
+     */
+    public IAction getJoinAction() {
 
-	@Override
-	public void createNewGame() {
-		//add player to list of existing game
-                PlayerInfo player = new PlayerInfo();
-                ClientCommunicatorFascadeSettlersOfCatan client = new ClientCommunicatorFascadeSettlersOfCatan();
-                String cookie = "";
-                
-                for(java.util.Map.Entry<Integer, String> cookieValue : client.getCookies().entrySet()){
-                    cookie = cookieValue.getValue();
+        return joinAction;
+    }
+
+    /**
+     * Sets the action to be executed when the user joins a game
+     *
+     * @param value The action to be executed when the user joins a game
+     */
+    public void setJoinAction(IAction value) {
+
+        joinAction = value;
+    }
+
+    public INewGameView getNewGameView() {
+
+        return newGameView;
+    }
+
+    public void setNewGameView(INewGameView newGameView) {
+
+        this.newGameView = newGameView;
+    }
+
+    public ISelectColorView getSelectColorView() {
+
+        return selectColorView;
+    }
+
+    public void setSelectColorView(ISelectColorView selectColorView) {
+
+        this.selectColorView = selectColorView;
+    }
+
+    public IMessageView getMessageView() {
+
+        return messageView;
+    }
+
+    public void setMessageView(IMessageView messageView) {
+
+        this.messageView = messageView;
+    }
+
+    @Override
+    public void start() {
+
+        getJoinGameView().showModal();
+    }
+
+    @Override
+    public void startCreateNewGame() {
+
+        getNewGameView().showModal();
+    }
+
+    @Override
+    public void cancelCreateNewGame() {
+
+        getNewGameView().closeModal();
+    }
+
+    @Override
+    public void createNewGame() {
+        try {
+            //add player to list of existing game
+            PlayerInfo player = new PlayerInfo();
+            ClientCommunicatorFascadeSettlersOfCatan client = new ClientCommunicatorFascadeSettlersOfCatan();
+
+            player.setName(new GsonBuilder().create().fromJson(URLDecoder.decode(client.getCookies().get(0).split("=")[1], "UTF-8"), CookieModel.class).getName());
+            player.setPlayerIndex(0);
+            Random rand = new Random();
+
+            //need a random number for the player index (unique..period)
+            int playerID = rand.nextInt();
+            boolean unique = true;
+            while (unique) {
+                for (GameInfo game : games) {
+                    for (PlayerInfo checkPlayer : game.getPlayers()) {
+                        if (checkPlayer.getId() == playerID) {
+                            unique = false;
+                            break;
+                        }
+                    }
+                    if (!unique) {
+                        break;
+                    }
                 }
-                System.out.println("Cookie: " + client.getCookies());
-                //player.setName(new GsonBuilder().create().fromJson(cookie.split("=")[1], CookieModel.class).getName());
-                player.setPlayerIndex(0);
-                Random rand = new Random();
-                //need a random number for the player index (unique)
-                player.setId(rand.nextInt());
-                
-                //set GameInfo for new Game
-                GameInfo game = new GameInfo();
-                game.addPlayer(player);
-                game.setTitle(getNewGameView().getTitle());
-                game.setId(currentGameId);
-                
-                //clear all the New Game View options
-                getNewGameView().setTitle("");
-                getNewGameView().setRandomlyPlaceHexes(false);
-                getNewGameView().setRandomlyPlaceNumbers(false);
-                getNewGameView().setUseRandomPorts(false);
-                
-                //add game to list of games
-                games.add(currentGameId, game);
-                
-                
-                //update the list of games in the JoinGame View 
-                GameInfo[] newGames = new GameInfo[games.size()];
-                newGames = games.toArray(newGames);
-                getJoinGameView().setGames(newGames, player);
-                
-		getNewGameView().closeModal();
-                
-                //increment the game id
-                currentGameId++;
-	}
+            }
+            player.setId(playerID);
 
-	@Override
-	public void startJoinGame(GameInfo game) {
+            //set GameInfo for new Game
+            GameInfo game = new GameInfo();
+            game.addPlayer(player);
+            game.setTitle(getNewGameView().getTitle());
+            game.setId(currentGameId);
 
-		getSelectColorView().showModal();
-	}
+            //clear all the New Game View options
+            getNewGameView().setTitle("");
+            getNewGameView().setRandomlyPlaceHexes(false);
+            getNewGameView().setRandomlyPlaceNumbers(false);
+            getNewGameView().setUseRandomPorts(false);
 
-	@Override
-	public void cancelJoinGame() {
-	
-		getJoinGameView().closeModal();
-	}
+            //add game to list of games
+            games.add(currentGameId, game);
 
-	@Override
-	public void joinGame(CatanColor color) {
-		
-		// If join succeeded
-		getSelectColorView().closeModal();
-		getJoinGameView().closeModal();
-		joinAction.execute();
-	}
+            //update the list of games in the JoinGame View 
+            GameInfo[] newGames = new GameInfo[games.size()];
+            newGames = games.toArray(newGames);
+            getJoinGameView().setGames(newGames, player);
+
+            getNewGameView().closeModal();
+
+            //increment the game id
+            currentGameId++;
+        } catch (UnsupportedEncodingException ex) {
+            Logger.getLogger(JoinGameController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public void startJoinGame(GameInfo game) {
+
+        getSelectColorView().showModal();
+    }
+
+    @Override
+    public void cancelJoinGame() {
+
+        getJoinGameView().closeModal();
+    }
+
+    @Override
+    public void joinGame(CatanColor color) {
+
+        // If join succeeded
+        getSelectColorView().closeModal();
+        getJoinGameView().closeModal();
+        joinAction.execute();
+    }
 
 }
-
