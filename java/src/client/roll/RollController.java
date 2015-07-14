a18ec9252b02b1a44c5d70839498a2c58a246331
= <=>
2015-07-13 18:55:26 -0600

    Roll Controller

--- java/src/client/roll/RollController.java
+++ java/src/client/roll/RollController.java
@@ -1,6 +1,13 @@
 package client.roll;
 
+import java.util.Random;
+
 import client.base.*;
+import client.communication.ClientCommunicator;
+import client.communication.ClientCommunicatorFascadeSettlersOfCatan;
+import client.managers.GameManager;
+import client.proxy.FinishMove;
+import client.proxy.RollNumber;
 
 
 /**
@@ -26,6 +33,7 @@ public class RollController extends Controller implements IRollController {
 	public IRollResultView getResultView() {
 		return resultView;
 	}
+	
 	public void setResultView(IRollResultView resultView) {
 		this.resultView = resultView;
 	}
@@ -36,8 +44,20 @@ public class RollController extends Controller implements IRollController {
 	
 	@Override
 	public void rollDice() {
+		GameManager gm = ClientCommunicator.getSingleton().getGameManager();
+		RollNumber rn = new RollNumber(gm.getGame().getTurnTracker().getCurrentTurn());
+		Random r = new Random();
+		int twoD6 = r.nextInt(6) + 1;
+		rn.setNumber(twoD6);
 
-		getResultView().showModal();
+		try {
+			gm.initializeGame(ClientCommunicatorFascadeSettlersOfCatan.getSingleton().rollNumber(rn));
+			getResultView().setRollValue(twoD6);
+			getRollView().closeModal();
+			getResultView().showModal();
+		} catch(Exception e) {
+			System.out.println("Couldn't end turn");
+		}	
 	}
 
 }
