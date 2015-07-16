ec74afca622552490aa4e100e935ca2686ebe4dc
aaron monson <aaron@lmonson.com>
2015-07-15 22:11:11 -0600

    Progress is being made on MapPoller

--- java/src/client/map/MapPoller.java
+++ java/src/client/map/MapPoller.java
@@ -11,54 +11,75 @@ import client.managers.GameManager;
 import client.communication.ClientCommunicator;
 import client.communication.ClientCommunicatorFascadeSettlersOfCatan;
 import client.map.MapController;
+import shared.definitions.PieceType;
 
 /**
  *
  * @author Samuel
  */
-public class MapPoller extends TimerTask{
-    
-    private MapController mapController;
-    private int version;
-    private boolean firstInitialization;
-    private int playerIndex;
-    
-    
-    public MapPoller(MapController mapController){
-        super();
-        this.mapController = mapController;
-        this.firstInitialization = false;
-        playerIndex=-1;
-        version=-1;
-    }
-    
-    public void run(){
-        if(ClientCommunicator.getSingleton().getJoinedGame()){
-            try{
-	            GameInfo gameInformation = ClientCommunicatorFascadeSettlersOfCatan.getSingleton().getGameModel(version+"");
-	            version = gameInformation.getVersion();
-	            ClientCommunicator.getSingleton().getGameManager().initializeGame(gameInformation);
-	            if(playerIndex==-1){
-	            	GameManager gm = ClientCommunicator.getSingleton().getGameManager();
-	                Integer playerId = ClientCommunicator.getSingleton().getPlayerId();
-	                for(int i=0;i<gm.getGame().getPlayers().size();i++){
-	                    if(gm.getGame().getPlayers().get(i).getId()==playerId){
-	                        playerIndex=i;
-	                        break;
-	                    }
-	                }
-	            }
-	            //If they haven't initialized before or it isn't the client's turn
-	            if (!firstInitialization) {
-	            	firstInitialization = true;
-	            	mapController.initFromModel();
-	            }
-	            
-	            //mapController.initFromModel();
-            }catch (Exception e){
-            }
-        }
-//        System.out.println("meow");
-    }
-    
+public class MapPoller extends TimerTask {
+
+	private MapController mapController;
+	private int version;
+	private boolean firstInitialization;
+	private int playerIndex;
+
+	public MapPoller(MapController mapController) {
+		super();
+		this.mapController = mapController;
+		this.firstInitialization = false;
+		playerIndex = -1;
+		version = -1;
+	}
+
+	public void run() {
+		if (ClientCommunicator.getSingleton().getJoinedGame()) {
+			try {
+				GameInfo gameInformation = ClientCommunicatorFascadeSettlersOfCatan.getSingleton()
+						.getGameModel(version + "");
+				version = gameInformation.getVersion();
+				ClientCommunicator.getSingleton().getGameManager().initializeGame(gameInformation);
+				if (playerIndex == -1) {
+					GameManager gm = ClientCommunicator.getSingleton().getGameManager();
+					Integer playerId = ClientCommunicator.getSingleton().getPlayerId();
+					for (int i = 0; i < gm.getGame().getPlayers().size(); i++) {
+						if (gm.getGame().getPlayers().get(i).getId() == playerId) {
+							playerIndex = i;
+							break;
+						}
+					}
+				}
+				// If they haven't initialized before or it isn't the client's
+				// turn
+				if (!firstInitialization || gameInformation.getTurnTracker().getCurrentTurn() != playerIndex) {
+					mapController.initFromModel();
+					GameManager gm = ClientCommunicator.getSingleton().getGameManager();
+
+					System.out.println(gameInformation.getTurnTracker().getCurrentTurn() + " ->? " + playerIndex);
+					if (gameInformation.getTurnTracker().getCurrentTurn() != playerIndex)
+						// This boolean toggles on after your turn, so when the
+						// turn track comes around again, you get exactly one
+						// update on each of your turns. This way, your client
+						// will switch to a "It is your turn" state.
+						firstInitialization = false;
+					else {
+						firstInitialization = true;
+						if (gm.getLocationManager().getSettledLocations().size() < 4) {
+							// We are on the first round.
+							mapController.startMove(PieceType.SETTLEMENT, true, true);
+							mapController.startMove(PieceType.ROAD, false, true);
+						} else if (gm.getLocationManager().getSettledLocations().size() < 8) {
+							// We are on the second round.
+							mapController.startMove(PieceType.SETTLEMENT, true, true);
+							mapController.startMove(PieceType.ROAD, false, true);
+						}
+					}
+
+				}
+			} catch (Exception e) {
+			}
+		}
+		// System.out.println("meow");
+	}
+
 }
