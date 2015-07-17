e5c0b04e1211dc19c98d21d353da3028a8a87e1f
Samuel Lyons <PestytheRat@gmail.com>
2015-07-16 22:43:09 -0600

    stuff

--- java/src/client/turntracker/TurnTrackerController.java
+++ java/src/client/turntracker/TurnTrackerController.java
@@ -28,7 +28,19 @@ public class TurnTrackerController extends Controller implements ITurnTrackerCon
 	@Override
 	public void endTurn() {
                GameManager gm = ClientCommunicator.getSingleton().getGameManager();
+                Integer playerId = ClientCommunicator.getSingleton().getPlayerId();
+                Integer playerIndex = 4;
+                for(int i=0;i<gm.getGame().getPlayers().size();i++){
+                    if(gm.getGame().getPlayers().get(i).getPlayerID()==playerId){
+                        playerIndex=i;
+                        break;
+                    }
+                }
+                
 		FinishMove fm = new FinishMove(gm.getGame().getTurnTracker().getCurrentTurn());
+                fm.setType("finishTurn");
+                fm.setPlayerIndex(playerIndex);
+                
 		try {
 			gm.initializeGame(ClientCommunicatorFascadeSettlersOfCatan.getSingleton().finishMove(fm));
 			getView().updateGameState("Waiting for other players", false);
