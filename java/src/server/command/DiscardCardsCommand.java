c6c5b1f07f4e788106d67f02baac27eec0c7dd3d
Samuel Lyons <PestytheRat@gmail.com>
2015-07-27 15:16:10 -0600

    discarding

--- java/src/server/command/DiscardCardsCommand.java
+++ java/src/server/command/DiscardCardsCommand.java
@@ -24,9 +24,26 @@ public class DiscardCardsCommand implements Command {
     @Override
     public boolean execute() {
         try {
+            AllOfOurInformation.getSingleton().getGames().get(discardCards.getGameId()).getGame().getPlayers().get(discardCards.getPlayerIndex()).setDiscarded(true);
             AllOfOurInformation.getSingleton().getGames().get(discardCards.getGameId()).getResourceManager().transferResourceCard(discardCards.getPlayerIndex(), 4, discardCards.getDiscardedCards());
             AllOfOurInformation.getSingleton().getGames().get(discardCards.getGameId()).saveResourcesIntoGame();
-            AllOfOurInformation.getSingleton().getGames().get(discardCards.getGameId()).getGame().setVersion(AllOfOurInformation.getSingleton().getGames().get(discardCards.getGameId()).getGame().getVersion()+1);
+            boolean everyoneDiscarded = true;
+            for (int i = 0; i < 4; i++) {
+                if (!(AllOfOurInformation.getSingleton().getGames().get(discardCards.getGameId()).getGame().getPlayers().get(i).isDiscarded() ||
+                        AllOfOurInformation.getSingleton().getGames().get(discardCards.getGameId()).getGame().getPlayers().get(i).getResources().getTotalResources() < 7)) {
+                    everyoneDiscarded = false;
+                }
+            }
+
+            if (everyoneDiscarded) {
+                for (int i = 0; i < 4; i++) {
+                    AllOfOurInformation.getSingleton().getGames().get(discardCards.getGameId()).getGame().getPlayers().get(i).setDiscarded(false);
+                }
+                AllOfOurInformation.getSingleton().getGames().get(discardCards.getGameId()).getGame().getTurnTracker().setStatus("Robbing");
+            }
+
+            AllOfOurInformation.getSingleton().getGames().get(discardCards.getGameId()).getGame().setVersion(AllOfOurInformation.getSingleton().getGames().get(discardCards.getGameId()).getGame().getVersion() + 1);
+
             return true;
         } catch (Exception e) {
             return false;
